ba932488fe1f800097173b7d29628ce2c5c7bac9
Vero <verotorgom.12@gmail.com>
2023-05-26 00:32:28 +0200

    video NO PUDE SUBIRLO ES MUY PESADO  y cositas 2

---
+++ BlazorWeb/PoroConsultor/wwwroot/js/Index.js
@@ -0,0 +1,27 @@
+﻿const imagenes = document.querySelectorAll(".imagen");
+export function checkScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (let i = 0; i < imagenes.length; i++) {
+        let alturaImagen = imagenes[i].offsetTop;
+        if (alturaImagen - 500 < scrollTop) {
+            imagenes[i].style.opacity = 1;
+            imagenes[i].classList.add("mostrarDerecha");
+        }
+    }
+    
+};
+window.addEventListener('scroll', checkScroll);
+
+/*const video = document.getElementByTagName("video")[0];*/
+//window.addEventListener('DOMContentLoaded', function () {
+//    var video = document.getElementById('myVideo'); // Reemplaza 'myVideo' con el ID de tu elemento de video
+//});
+//export function cargarVideo() {
+    
+
+//        video.addEventListener('loadedmetadata', function () {
+//            video.loop = true; // Establece la propiedad loop en true para que se reproduzca en bucle
+//            video.play(); // Inicia la reproducción del video
+//        });
+//}
+
