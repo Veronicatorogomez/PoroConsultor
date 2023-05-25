88d5fe1f664bbd9bef28019491655200e52341eb
Oliver <oliver22official@gmail.com>
2023-05-26 01:58:13 +0200

    rehice api stats de 0

--- BlazorWeb/PoroConsultor/Models/Estadisticas.cs
+++ BlazorWeb/PoroConsultor/Models/Estadisticas.cs
@@ -2,21 +2,13 @@
 public class Estadisticas
 {
 	public string Nombre { get; set; }
-	public string PorcentajeAD { get; set; }
-	public string PorcentajeAP { get; set; }
-	public string PorcentajeTD { get; set; }
 	public string Vida { get; set; }
 	public string Mana { get; set; }
 	public string Dano { get; set; }
 	public string Armadura { get; set; }
 	public string ResistenciaMagica { get; set; }
-	public string TipoEnergia { get; set; }
-	public string VidaRegen { get; set; }
-	public string ManaRegen { get; set; }
 	public string Alcance { get; set; }
-	public string ProbabilidadCritico { get; set; }
 	public string Velocidad { get; set; }
 	public string VelocidadDeAtaque { get; set; }
 	public Estadisticas() { }
-
 }
--- BlazorWeb/PoroConsultor/Models/Union.cs
+++ BlazorWeb/PoroConsultor/Models/Union.cs
@@ -15,23 +15,16 @@ public class Union
     public List<string> IconoSkin { get; set; }
     public List<string> NombreSkin { get; set; }
     public List<string> ImagenSkin { get; set; }
-    public string PorcentajeAD { get; set; }
-    public string PorcentajeAP { get; set; }
-    public string PorcentajeTD { get; set; }
-    public string Vida { get; set; }
-    public string Mana { get; set; }
-    public string Dano { get; set; }
-    public string Armadura { get; set; }
-    public string ResistenciaMagica { get; set; }
-    public string TipoEnergia { get; set; }
-    public string VidaRegen { get; set; }
-    public string ManaRegen { get; set; }
-    public string Alcance { get; set; }
-    public string ProbabilidadCritico { get; set; }
-    public string Velocidad { get; set; }
-    public string VelocidadDeAtaque { get; set; }
+	public string Vida { get; set; }
+	public string Mana { get; set; }
+	public string Dano { get; set; }
+	public string Armadura { get; set; }
+	public string ResistenciaMagica { get; set; }
+	public string Alcance { get; set; }
+	public string Velocidad { get; set; }
+	public string VelocidadDeAtaque { get; set; }
 
-    public Union()
+	public Union()
     {
 
     }
@@ -51,19 +44,12 @@ public class Union
 		IconoSkin = campeon.IconoSkin;
 		NombreSkin = campeon.NombreSkin;
 		ImagenSkin = campeon.ImagenSkin;
-		PorcentajeAD = stat.PorcentajeAD;
-        PorcentajeAP = stat.PorcentajeAP;
-        PorcentajeTD = stat.PorcentajeTD;
         Vida = stat.Vida;
         Mana = stat.Mana;
         Dano = stat.Dano;
         Armadura = stat.Armadura;
 		ResistenciaMagica = stat.ResistenciaMagica;
-		TipoEnergia = stat.TipoEnergia;
-		VidaRegen = stat.VidaRegen;
-		ManaRegen = stat.ManaRegen;
 		Alcance = stat.Alcance;
-        ProbabilidadCritico = stat.ProbabilidadCritico;
         Velocidad = stat.Velocidad;
 		VelocidadDeAtaque = stat.VelocidadDeAtaque;
 	}
