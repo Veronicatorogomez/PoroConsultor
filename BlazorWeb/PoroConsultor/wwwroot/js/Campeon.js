9133160a4d3610aad0c8cccf581bffee1b55ae40
Oliver <oliver22official@gmail.com>
2023-05-26 21:56:02 +0200

    muteao

--- BlazorWeb/PoroConsultor/wwwroot/js/Campeon.js
+++ BlazorWeb/PoroConsultor/wwwroot/js/Campeon.js
@@ -1,5 +1,10 @@
 ﻿export function playVideoBucle() {
-    const video = document.getElementsByTagName("video")[0]
+    const video = document.getElementsByTagName("video")[0];
     video.play();
     console.log("hola")
+}
+
+export function muteVideo() {
+    const video = document.getElementsByTagName("video")[0];
+    video.muted = true;
 }
